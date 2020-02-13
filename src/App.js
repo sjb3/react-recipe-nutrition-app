535b0b20ad5836d1a9a54bcabac2da6d735514c7
Justin Byun <klofseattle@hotmail.com>
2020-02-12 20:29:27 -0800

    Finish with styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import { Recipe } from './components/Recipe';
+import { Recipe } from "./components/Recipe";
+import { Button, TextField } from '@material-ui/core';
 
 function App() {
   const APP_ID = "8160aeca";
   const APP_KEY = "4ea095d280a0984d221c7642968d6c54	";
-  const reqUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('korean');
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await axios.get(reqUrl);
+    const response = await axios.get(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     // const data = await response;
     setRecipes(response.data.hits);
     console.log(response.data.hits);
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
-        <button className="search-btn" type="submit">
+      <h1 className='main-title'>Recipe Search!</h1>
+      <form className="search-form" onSubmit={getSearch}>
+        <TextField required id="search-bar" label='Search...' type="text" value={search} onChange={updateSearch} />
+        <Button variant="contained" color="primary" className="search-btn" type="submit">
           Search
-        </button>
+        </Button>
       </form>
+      <div className='recipes'>
       {recipes.map(recipe => (
         // <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
-        <Recipe recipe={recipe} />
+        <Recipe recipe={recipe} key={recipe.recipe.label} />
       ))}
+      </div>
     </div>
   );
 }
